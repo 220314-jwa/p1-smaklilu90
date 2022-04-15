979f206bdcf5016ff3482e2d689c2068cef55da7
smaklilu90 <47577332+smaklilu90@users.noreply.github.com>
2022-04-15 00:39:59 -0500

    Delete spms-app directory

--- spms-app/src/main/java/com/revature/exceptions/IncorrectCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.exceptions;
-
-public class IncorrectCredentialsException extends Exception {
-
-	public IncorrectCredentialsException() {
-		super("The username and/or password were incorrect.");
-	}
-}
